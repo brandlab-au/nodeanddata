a31e81719e537e3951dbc008e9aeab9b563f0c71
Jon Mckenzies <jon@brandlab.co.uk>
2016-09-09 12:14:24 -0500

    start

---
+++ websitetitle.js
@@ -0,0 +1,14 @@
+var title = "Jon's Landscapes";
+
+var websiteTitle = {
+
+  setTitle : function(newTitle) {
+    title = newTitle;
+  },
+  getTitle : function() {
+    return title;
+  }
+
+}
+
+module.exports = websiteTitle;
